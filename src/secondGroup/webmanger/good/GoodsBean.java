d995e5a0d6524430ee0ff6387f2e11585a9fd27b
GaoYanZe <45003314+GaoYanZe@users.noreply.github.com>
2018-11-23 19:38:04 +0800

    创建商品类
    创建商品类JavaBean，包括商品编号，名称，数量，价格，生产商，发货地址，生产日期，保质期，月销量

---
+++ src/secondGroup/webmanger/good/GoodsBean.java
@@ -0,0 +1,82 @@
+package secondGroup.webmanger.good;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class GoodsBean {
+
+	private String goodsNo;
+	private String goodsName;
+	private BigDecimal goodsPrice;
+	private String producer;
+	private String goodsDeliveryAddress;
+	private int goodsMonthSale;
+	private int goodsnumber;
+	private Date productDate;
+	private Date expriDate;
+	private int type;
+	
+	public String getProducer() {
+		return producer;
+	}
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+	public Date getProductDate() {
+		return productDate;
+	}
+	public void setProductDate(Date productDate) {
+		this.productDate = productDate;
+	}
+	public Date getExpriDate() {
+		return expriDate;
+	}
+	public void setExpriDate(Date expriDate) {
+		this.expriDate = expriDate;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public void setGoodsPrice(BigDecimal goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getGoodsNo() {
+		return goodsNo;
+	}
+	public void setGoodsNo(String goodsNo) {
+		this.goodsNo = goodsNo;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public BigDecimal getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice1(BigDecimal goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getGoodsDeliveryAddress() {
+		return goodsDeliveryAddress;
+	}
+	public void setGoodsDeliveryAddress(String goodsDeliveryAddress) {
+		this.goodsDeliveryAddress = goodsDeliveryAddress;
+	}
+	public int getGoodsMonthSale() {
+		return goodsMonthSale;
+	}
+	public void setGoodsMonthSale(int goodsMonthSale) {
+		this.goodsMonthSale = goodsMonthSale;
+	}
+	public int getGoodsnumber() {
+		return goodsnumber;
+	}
+	public void setGoodsnumber(int goodsnumber) {
+		this.goodsnumber = goodsnumber;
+	}
+}
